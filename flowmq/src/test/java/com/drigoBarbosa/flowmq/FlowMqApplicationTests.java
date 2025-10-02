6f8fb837f44788951ea4d6df9f46f036805d8b2e
Rodrigo Barbosa <rodrigobarbosa@ypcontrol.com>
2025-10-02 13:24:35 -0300

    feat: Criação do projeto

---
+++ flowmq/src/test/java/com/drigoBarbosa/flowmq/FlowMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drigoBarbosa.flowmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
